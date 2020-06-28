637d8263bf8654004d3f89a1caae6d8bf5363454
Hannah Jenkins <hannahbrittany@hotmail.com>
2020-06-28 16:35:20 -0400

    added question upon start click with array, updated questions

--- assets/quiz.js
+++ assets/quiz.js
@@ -11,7 +11,7 @@ const startButton = document.getElementById('start-btn-id')
 const startContainer =document.getElementById('start-btn-container')
 const questionContainer = document.getElementById('quiz')
 const questionElement = document.getElementById('question')
-const answerElement = document.getElementById('answer-buttons')
+const answerElement = document.getElementById('answer')
 
 startButton.addEventListener("click", startGame)
 
@@ -24,23 +24,62 @@ function startGame() {
 }
 
 function nextQuestion(question) {
-    questionElement.innerText = questions["question"];
+    questionElement.innerText = questions[0]["question"];
+    answerElement.innerText = questions[0]["answers"];
 }
 
 function selectAnswer() {
 
 }
 
+
 //this is an array of questions
 const questions = [
     {
-        question: "What is 2+2",
+        question: "The condition in an if / else statement is enclosed with _____.",
         answers: [
-            { text: "4", correct: true },
-            { text: "22", correct: false },
-            { text: "45", correct: false },
-            { text: "11", correct: false },
+            { text: "quotes", correct: false },
+            { text: "curly brackets", correct: false },
+            { text: "parenthases", correct: true },
+            { text: "square brackets", correct: false },
+        ]
+    },
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        answers: [
+            { text: "<scripting>", correct: false },
+            { text: "<script>", correct: true },
+            { text: "You can put it anywhere", correct: false },
+            { text: "<style>", correct: false },
+        ]
+    },
+    {
+        question: "How do you write 'Hello World' in an alert box?",
+        answers: [
+            { text: 'alert("Hello World");', correct: true },
+            { text: 'msg("Hello World");', correct: false },
+            { text: 'alertBox("Hello World");', correct: false },
+            { text: 'msgBox("Hello World");', correct: false },
+        ]
+    },
+    {
+        question: "How does a FOR loop start?",
+        answers: [
+            { text: "for (i=0; i <= 5;i++)", correct: true },
+            { text: "for i=1 to 5", correct: false },
+            { text: "for (i<=5; i++)", correct: false },
+            { text: "for (i=0; i <= 5)", correct: false },
+        ]
+    },
+    {
+        question: "How can you add a comment in JavaScript?",
+        answers: [
+            { text: "<!--This is a comment-->", correct: false },
+            { text: "'This is a comment", correct: false },
+            { text: "(This is a comment)", correct: false },
+            { text: "//This is a comment", correct: true },
         ]
     }
+
 
 ]
